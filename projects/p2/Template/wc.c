776748c581c080f0fa22f7d46d192a4be28228a5
Gresh Basic <basic.gresham@gmail.com>
2024-02-16 20:41:56 -0600

    wc done

--- projects/p2/Template/wc.c
+++ projects/p2/Template/wc.c
@@ -15,7 +15,7 @@ int wc_l(char* path){
 		perror("wc");
 	}
 
-	read = getline(&line, &len, file);
+	read = getline(&line, &len, file);			// keep getline'ing lines until there are none left
 	while (read >= 0) {
 		line_count += 1;
 		read = getline(&line, &len, file);
@@ -27,19 +27,55 @@ int wc_l(char* path){
 }
 
 int wc_w(char* path){
+	// coding in c is so much fun
+	//       ^  ^ ^  ^  ^    ^
+	// hello world!
+	//      ^
+	// banana
+
+	// word count = num_spaces + 1?
+	// ^ as long as file isn't blank
+
 	int word_count = 0;
+	FILE *file;
+	char curr_char;
+	char prev_char;
 
+	file = fopen(path, "r");
+	if (!file) {
+		perror("wc");
+	}
 
-
+	curr_char = fgetc(file);
+	while (curr_char != EOF) {								//											     |		
+		if (curr_char == ' ' || curr_char == '\n') {	    // found a space or newline						 v	
+			if (prev_char != ' ' && prev_char != '\n') {    // is this a space AFTER a character? ("character ")
+				word_count += 1;							// if so, then we have seen +1 words
+			}
+		}
+		prev_char = curr_char;
+		curr_char = fgetc(file);
+	}
 
 	return word_count;
 }
 
 int wc_c(char* path){
 	int char_count = 0;
+	FILE *file;
+	char curr_char;
+	char prev_char;
 
+	file = fopen(path, "r");
+	if (!file) {
+		perror("wc");
+	}
 
-
+	curr_char = fgetc(file);		// using same idea as wc_l, just keep fgetc'ing chars until there are none left
+	while (curr_char != EOF) {	
+		char_count += 1;
+		curr_char = fgetc(file);
+	}
 
 	return char_count;
 }
@@ -53,13 +89,13 @@ void wc(int mode, char* path){
 
 	if (mode == 1) {
 		line_count = wc_l(path);
-		printf(" %d %s\n", line_count, path);
+		printf("%d %s\n", line_count, path);
 	} else if (mode == 2) {
 		word_count = wc_w(path);
-		printf(" %d %s\n", word_count, path);
+		printf("%d %s\n", word_count, path);
 	} else if (mode == 3) {
 		char_count = wc_c(path);
-		printf(" %d %s\n", char_count, path);
+		printf("%d %s\n", char_count, path);
 	} else {
 		line_count = wc_l(path);
 		word_count = wc_w(path);
