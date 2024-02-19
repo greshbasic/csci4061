01f889e87af9295bdee2e256e1edc318d69edab1
Gresh Basic <basic.gresham@gmail.com>
2024-02-18 22:06:38 -0600

    lab5 done

--- labs/lab5/template/chain_pipe.c
+++ labs/lab5/template/chain_pipe.c
@@ -20,26 +20,26 @@ You will have to work a bit more to figure out certain small details
 int main(int argc, char *argv[]){
     // accept single argument and convert to integer n. n is number of processes
     // atoi stands for ascii to integer
-    int n = atoi(???);
+    int n = atoi(argv[1]);
 
     pid_t pid;
     char data_from_parent[1024] = "";
     for(int i = 1; i < n; i++){
         int fd[2];
         // create the pipe using pipe() function and fd[2] array above
-        ???
+        pipe(fd);
 
         pid = fork();
 
         if(pid != 0){ // parent
             // close read end of pipe
-            ???
+            close(fd[0]);
             
             // write pid to the write end of pipe
             int id = getpid();
-            ???
+            write(fd[1], &id, sizeof(id));
             // close write end
-            ???
+            close(fd[1]);
 
             wait(NULL);
             break;
@@ -49,16 +49,16 @@ int main(int argc, char *argv[]){
             char str_pid[10] = "";
 
             // close write end
-            ???
+            close(fd[1]);
 
             // read pid from read end of pipe and store it into `data_from_parent` buffer
-            read(???);
+            read(fd[0], &pid_from_parent, sizeof(pid_from_parent));
             sprintf(str_pid, "%d", pid_from_parent);
             strcat(data_from_parent, str_pid);
             strcat(data_from_parent, " ");
 
             // close read end
-            ???
+            close(fd[0]);
 
             if(i == n - 1){// If this is the last child process
                 // Get current PID and store into the `data_from_parent` buffer
@@ -69,20 +69,19 @@ int main(int argc, char *argv[]){
 
                 // STDOUT_FILENO is a macro that represents the file descriptor number of the standard output, usually terminal/console
                 // Duplicate STDOUT_FILENO into TEMP_STDOUT so that we can resotre STDOUT_FILENO later
-                int TEMP_STDOUT = dup(???);
+                int TEMP_STDOUT = dup(STDOUT_FILENO);
 
                 // Open the out.txt file using permission given at the top
-                int out_fd = open(???);
-
+                int out_fd = open("out.txt", O_WRONLY | O_CREAT | O_TRUNC);
                 // Redirect out_fd to STDOUT_FILENO
-                int ret_val = dup2(???);
+                int ret_val = dup2(out_fd, STDOUT_FILENO);
 
                 // Print `data_from_parent`. Because we Redirect fd to STDOUT_FILENO, this should go to the out.txt
                 printf("%s", data_from_parent);
                 fflush(stdout); // this statement force the program output the data immediatly.
 
                 // restore STDOUT_FILENO using TEMP_STDOUT
-                ret_val = dup2(???);
+                ret_val = dup2(TEMP_STDOUT, STDOUT_FILENO);
                 // This should go to the terminal
                 printf("%s\n", data_from_parent);
 
